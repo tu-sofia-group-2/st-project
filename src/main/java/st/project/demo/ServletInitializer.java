a5ef8a8a1b4260d637c22fbae8882bb77ca24121
Krasimir MArinov <kra.marinov@gmail.com>
2022-11-12 13:21:56 +0200

    Initialise project basics

---
+++ src/main/java/st/project/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package st.project.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+}
