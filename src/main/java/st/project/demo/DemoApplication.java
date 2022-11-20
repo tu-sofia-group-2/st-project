102b04a915cd203cccf2ee3b99e0af3119d6d4f6
Krikchou <31549619+Krikchou@users.noreply.github.com>
2022-11-20 19:52:34 +0200

    Remove changes from another issue

--- src/main/java/st/project/demo/DemoApplication.java
+++ src/main/java/st/project/demo/DemoApplication.java
@@ -1,10 +1,7 @@
 package st.project.demo;
 
-import org.apache.shiro.realm.Realm;
-import org.apache.shiro.realm.jdbc.JdbcRealm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
