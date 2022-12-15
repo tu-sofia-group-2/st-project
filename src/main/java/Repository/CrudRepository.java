f990ba5d8e987c57d7b7a97d2f3953af9e9b1629
Krikchou <31549619+Krikchou@users.noreply.github.com>
2022-12-15 20:59:08 +0200

    ?????????????????????????

--- src/main/java/Repository/CrudRepository.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-import org.springframework.data.repository.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public interface CrudRepository<T, ID> extends Repository<T, ID> {
-}
